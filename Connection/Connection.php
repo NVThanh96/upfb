dd8f4962774013daedb15fe2bfc54a9c3541d196
Nguyen Van Thanh <toilatoi8624@gmail.com>
2023-08-28 09:57:32 +0700

    new

---
+++ Connection/Connection.php
@@ -0,0 +1,23 @@
+<?php
+class Connection
+{
+    private static $dsn = 'mysql:hosts=localhost;dbname=project_php';
+    private static $username = "root";
+    private static $password = "";
+    private static $options = [
+        \PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+        \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION
+    ];
+
+    private static $db;
+
+    public static function getDB()
+    {
+        try {
+            self::$db = new \PDO(self:: $dsn, self::$username, self::$password, self::$options);
+            return self::$db;
+        } catch (PDOException $e) {
+            echo "Error connect Database: " . $e->getMessage();
+        }
+    }
+}
